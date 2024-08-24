737aca6b323fc8f628f555201be0222c9cd67801
jugendragangwar <jugendragangwar8791@gmail.com>
2024-08-24 22:43:22 +0530

    latest Commit

--- src/Component/TodoForm.jsx
+++ src/Component/TodoForm.jsx
@@ -2,22 +2,12 @@ import React, { useState,useEffect} from "react";
 import { MdDelete } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
 
+
+
 const TodoForm = () => {
   const [value, setValue] = useState("")
   const [todo, setTodo] = useState([])
-
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem('todo'));
-    if (storedTodos) {
-      setTodo(storedTodos);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('todo', JSON.stringify(todo));
-  }, [todo]);
-
-
+  
   const addTodo = (e) => {
     e.preventDefault();
     if (value.trim()) {
@@ -28,8 +18,12 @@ const TodoForm = () => {
   const handleRemoveTodo = (index) => {
     const newTodos = todo.filter((_, i) => i !== index);
     setTodo(newTodos);
+    if (index === editIndex) {
+      setEditIndex(null);
+      setValue("");
+    }
   };
-
+  
   return (
     <div className='bg-slate-700 w-5/6 min-h-screen mx-auto'>
       <div className='text-white text-8xl text-center mb-8'>
@@ -38,29 +32,34 @@ const TodoForm = () => {
 
       <form className="flex justify-center">
         <input
-          className='w-3/6 h-10 px-4 rounded-l-2xl border-none bg-black text-white'
+          className='w-3/6 h-10 px-4 rounded-l-2xl bg-black text-white'
           type="text"
           value={value}
           placeholder="Add a Task"
           onChange={(e) => setValue(e.target.value)}
         />
-        <button className="bg-red-100 rounded-r-2xl w-20 "
+        <button className="bg-red-100 rounded-r-2xl w-20 hover:bg-slate-500 text-white"
           onClick={addTodo}>I Got this?</button>
       </form>
 
       {/* List Tailwind CSS */}
-      <div className=" my-10 ">
-        {todo.map((tod, index) => (
-          <li className="bg-slate-300 my-5 h-8 w-2/5 rounded-lg list-none flex justify-between" key={index} >
-            {tod}
-            <div className="flex items-center h-8 w-12">
-              <FaEdit />
-              <MdDelete onClick={() => handleRemoveTodo(index)} />
-            </div>
-          </li>
+      <div className="my-10 bg-slate-600 flex justify-center items-center">
+  <ul className="w-full max-w-xl">
+    {todo.map((tod, index) => (
+      <li
+        className="bg-slate-300 my-5 h-8 w-full rounded-lg list-none flex justify-between"
+        key={index}
+      >
+        {tod}
+        <div className="flex items-center h-8 w-12">
+          <FaEdit onClick={()=> handleEditTodo(index)} />
+          <MdDelete onClick={() => handleRemoveTodo(index)} />
+        </div>
+      </li>
+    ))}
+  </ul>
+</div>
 
-        ))}
-      </div>
     </div>
   )
 }
